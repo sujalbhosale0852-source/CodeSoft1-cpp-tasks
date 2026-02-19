dc27345aba5658beed63e75451dc9c760828e6ff
sujalbhosale0852-source <sujalbhosale0852@gmail.com>
2026-02-19 15:37:06 +0530

    Implement Tic Tac Toe game in C++ completed

---
+++ Task3/Tic Tac Toe game.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+using namespace std;
+
+char board[3][3];
+char currentPlayer;
+
+void resetBoard() {
+    char num = '1';
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            board[i][j] = num++;
+        }
+    }
+    currentPlayer = 'X';
+}
+
+void displayBoard() {
+    cout << "\n";
+    for (int i = 0; i < 3; i++) {
+        cout << " ";
+        for (int j = 0; j < 3; j++) {
+            cout << board[i][j];
+            if (j < 2) cout << " | ";
+        }
+        cout << "\n";
+        if (i < 2) cout << "---+---+---\n";
+    }
+    cout << "\n";
+}
+
+bool makeMove(int choice) {
+    int row = (choice - 1) / 3;
+    int col = (choice - 1) % 3;
+
+    if (choice < 1 || choice > 9 || board[row][col] == 'X' || board[row][col] == 'O') {
+        return false;
+    }
+
+    board[row][col] = currentPlayer;
+    return true;
+}
+
+bool checkWin() {
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == currentPlayer &&
+            board[i][1] == currentPlayer &&
+            board[i][2] == currentPlayer)
+            return true;
+
+        if (board[0][i] == currentPlayer &&
+            board[1][i] == currentPlayer &&
+            board[2][i] == currentPlayer)
+            return true;
+    }
+
+    if (board[0][0] == currentPlayer &&
+        board[1][1] == currentPlayer &&
+        board[2][2] == currentPlayer)
+        return true;
+
+    if (board[0][2] == currentPlayer &&
+        board[1][1] == currentPlayer &&
+        board[2][0] == currentPlayer)
+        return true;
+
+    return false;
+}
+
+bool checkDraw() {
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (board[i][j] != 'X' && board[i][j] != 'O')
+                return false;
+    return true;
+}
+
+void switchPlayer() {
+    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+}
+
+int main() {
+    char playAgain;
+
+    do {
+        resetBoard();
+        bool gameOver = false;
+
+        while (!gameOver) {
+            displayBoard();
+            int choice;
+
+            cout << "Player " << currentPlayer << ", enter your move (1-9): ";
+            cin >> choice;
+
+            if (!makeMove(choice)) {
+                cout << "Invalid move. Try again.\n";
+                continue;
+            }
+
+            if (checkWin()) {
+                displayBoard();
+                cout << "?? Player " << currentPlayer << " wins!\n";
+                gameOver = true;
+            }
+            else if (checkDraw()) {
+                displayBoard();
+                cout << "?? It's a draw!\n";
+                gameOver = true;
+            }
+            else {
+                switchPlayer();
+            }
+        }
+
+        cout << "Play again? (y/n): ";
+        cin >> playAgain;
+
+    } while (playAgain == 'y' || playAgain == 'Y');
+
+    cout << "Thanks for playing! ??\n";
+    return 0;
+}
